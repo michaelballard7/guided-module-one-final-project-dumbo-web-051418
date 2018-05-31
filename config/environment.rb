7e9c1eef4b0c02f3b503aca311dcd714a2080adc
github username <github email address>
2018-05-31 14:59:14 -0400

    We have methods

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ Bundler.require
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
+require_all 'app'
